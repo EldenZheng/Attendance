cba95c77afd33dce10279b9514b71116f53cc760
EldenZheng <elden.zheng@gmail.com>
2024-02-20 15:07:33 +0800

    ui fix 3

--- client/src/Attendance.jsx
+++ client/src/Attendance.jsx
@@ -112,7 +112,7 @@ export default function AttendanceList(){
 
     return(
         <div className="d-flex vh-100 vw-100 justify-content-center align-items-center bg-secondary-subtle">
-            <div className='w-70 bg-white rounded p-3'>
+            <div className='w-30 vh-70 bg-white rounded p-3'>
                 <a onClick={returnHome}><FontAwesomeIcon icon={faChevronLeft} />Back</a> 
                 <h4>Attendance List{filteredData && (<> - <a onClick={exportData}>Export to Excel</a></>)}</h4>
                 <Button variant="primary" onClick={() => setModalShow(true)}>
